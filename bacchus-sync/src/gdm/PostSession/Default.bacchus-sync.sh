5e552bc43f0348b159e5c016a32b506cc43305d5
YeonGyu Jeong <81887821@hanmail.net>
2018-11-01 19:13:46 +0900

    Excluded *.wdb and .cache from sync

--- bacchus-sync/src/gdm/PostSession/Default.bacchus-sync.sh
+++ bacchus-sync/src/gdm/PostSession/Default.bacchus-sync.sh
@@ -6,6 +6,8 @@ then
   exit 0
 fi
 
+# Evil Xilinx ISE files
+find "$HOME" -name '*.wdb' -type f -delete
 # Domain user: invoke domain user session cleanup
 /usr/bin/bacchus-sync --domain --logout
 
