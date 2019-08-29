ddd5791ab8fa87b865f84c9ed6b8dd868ad1587f
YeonGyu Jeong <81887821@hanmail.net>
2019-08-29 20:20:24 +0900

    Script bugfix

--- bacchus-system-updater/src/bacchus-common-script.sh
+++ bacchus-system-updater/src/bacchus-common-script.sh
@@ -17,13 +17,13 @@ function load_config() {
 function is_config_secure() {
 	local config_file="${1}"
 	local owner=$(stat --format '%u' "${config_file}") || die "Getting config file information failed"
-	local mode=$(stat --format '%f' "${config_file}") || die "Getting config file information failed"
+	local mode=$(stat --format '%a' "${config_file}") || die "Getting config file information failed"
 	
 	if [ "${#mode}" -eq "3" ]; then
 		local group_flags="${mode:1:1}"
 		local other_flags="${mode:2:1}"
 		
-		if [ "${owner}" -eq "0" ] && [ $"{group_flags}" -eq "0" ] && [ "${other_flags}" -eq "0" ]; then
+		if [ "${owner}" -eq "0" ] && [ "${group_flags}" -eq "0" ] && [ "${other_flags}" -eq "0" ]; then
 			return 0
 		else
 			return 1
